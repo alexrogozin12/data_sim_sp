cafc4a7c77ac7e058061c54441e556ff4ae62240
alexrogozin12 <aleksandr.rogozin@phystech.edu>
2022-02-11 14:40:47 +0300

    exp example

--- methods/minimization/__init__.py
+++ methods/minimization/__init__.py
@@ -2,6 +2,7 @@ from .base import BaseMethod
 from .bfgs import BFGS
 from .decentralized_gd import DecentralizedGD
 from .lbfgs import LBFGS
+from .logger import LoggerDecentralized
 from .gradient_descent import GradientDescent
 from .nesterov import Nesterov
 from .rbfgs import RBFGS
