1eda4b2142ab9c9866ccf191bf566b13763befb5
alexrogozin12 <aleksandr.rogozin@phystech.edu>
2022-02-14 22:55:31 +0300

    small

--- oracles/minimization/__init__.py
+++ oracles/minimization/__init__.py
@@ -1,3 +1,4 @@
 from .base import BaseSmoothOracle, OracleLinearComb
+from .linear_regression import LinearRegressionL2Oracle
 from .logistic import LogRegL2Oracle, create_log_reg_oracle
 from .quadratic import QuadraticOracle
